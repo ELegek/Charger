e4d17dc973b288a8f55811ae276af3c97c92f5cb
Evgeny <evgeny.legek@yandex.ru>
2020-05-08 00:16:55 +0300

    Адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function(){
 
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+
+
     $('.slider-header').slick ({
         arrows: true,
         fade: true,
@@ -16,35 +23,9 @@ $(function(){
         slidesToScroll: 1,
         autoplay: 3000,
         mobileFirst:true,
-        responsive: [
-          {
-            breakpoint: 1230,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: false,
-              
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
+        adaptiveHeight: true,
+        centerMode: true,
+        variableWidth: true,
     });
 
     $('.projects__slider2').slick ({
@@ -56,34 +37,9 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: 3000,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
+        adaptiveHeight: true,
+        centerMode: true,
+        variableWidth: true,
     });
 
 
